d1404a3c6e34533478e9b591a0757108dae2b93e
vaibha987 <v2goyal95@gmail.com>
2024-07-27 13:53:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function game() {
+	let playe1=document.getElementById("player-1")
+	let playe2=document.getElementById("player-2")
+	let mess=document.querySelector(".message")
+	let bt=document.getElementById("#submit")
+	mess.innerHTML="<h1>TIC TAC TOE</h1>" +playe1.value+", you're up"
+    
+}
